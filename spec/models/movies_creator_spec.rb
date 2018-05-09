e8510c8de9c66cf6dffc37ceea64a408312fec31
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2018-05-09 23:11:21 +0100

    WIP add more spec for MovieCreator

--- spec/models/movies_creator_spec.rb
+++ spec/models/movies_creator_spec.rb
@@ -13,5 +13,22 @@ describe MoviesCreator do
 
       expect(csv_importer).to have_received(:call)
     end
+
+    it "creates the mobies using the imported data" do
+      url = "http://test.com"
+      data = [{name: "Olalekan", email: "eyiolekan@gmail.com",
+               phone_number: "0123456", website: nil}]
+      movie_creator = MoviesCreator.new(url: url)
+      csv_importer = CsvImporter.new(url: url)
+      allow(csv_importer).to receive(:call)
+      allow(CsvImporter).to receive(:new).and_return(csv_importer)
+      allow(csv_importer).to receive(:result).and_return(data)
+
+      expect do
+        movie_creator.call
+      end.to change(User, :count).by(1)
+    end
   end
+
+
 end
